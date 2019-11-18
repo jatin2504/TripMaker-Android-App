f01f72e077437f2c3afa3a7be84a40e924cfbccd
Jatin Gupte <jatin.gupte25@gmail.com>
2019-11-18 01:28:21 -0500

    Created UI for login and registration screen.

--- app/src/main/java/com/example/tripmaker/MainActivity.java
+++ app/src/main/java/com/example/tripmaker/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.tripmaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RegistrationFragment.OnFragmentInteractionListener, LoginFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.mainActivityContainer, new RegistrationFragment(), "f_registration").commit();
+    }
+
+
+    @Override
+    public void onFragmentChange(String state) {
+        if (state == RegistrationFragment.FRAGMENT_STATE) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.mainActivityContainer, new LoginFragment(), "f_login").commit();
+        } else if (state == LoginFragment.FRAGMENT_STATE) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.mainActivityContainer, new RegistrationFragment(), "f_registration").commit();
+        }
     }
 }
---
+++ app/src/main/java/com/example/tripmaker/RegistrationFragment.java
@@ -0,0 +1,68 @@
+package com.example.tripmaker;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegistrationFragment extends Fragment {
+
+    public static final String FRAGMENT_STATE = "Registration";
+    private OnFragmentInteractionListener mListener;
+
+    public RegistrationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        getView().findViewById(R.id.loginTv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.onFragmentChange(FRAGMENT_STATE);
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentChange(String state);
+    }
+}
